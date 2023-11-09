36450ba4fc1d9ac9b92060e059cb2ca86ed4be47
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2023-11-09 14:16:40 +0100

    modifed

--- public/leaflet.js
+++ public/leaflet.js
@@ -1,9 +1,8 @@
 // create a variable for the map
-var map = L.map('map').setView([51.975, 7.61], 7);
+var map = L.map('map').setView([51.975, 7.61], 13);
 
 // add the base map
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 18,
 attribution: 'Leaflet, OpenStreetMap Contributors',
-}).addTo(map);
+maxZoom: 18}).addTo(map);
         
