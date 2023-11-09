904845e4f8bc82976f145fa9405c5a1da82c4c31
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2023-11-09 14:22:33 +0100

    leaflet draw elements

--- public/leaflet.js
+++ public/leaflet.js
@@ -5,4 +5,31 @@ var map = L.map('map').setView([51.975, 7.61], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: 'Leaflet, OpenStreetMap Contributors',
 maxZoom: 18}).addTo(map);
-        
+
+
+
+/**
+ * Layer on which the User can draw a shape
+ * @type Leaflet Layer
+ */
+let drawnItems = L.featureGroup().addTo(map);
+
+
+// Adding a Leaflet.Draw Toolbar
+map.addControl(new L.Control.Draw( {
+    edit: {
+        featureGroup: drawnItems,
+        poly: {
+            allowIntersection: false
+        }
+    },
+    draw: {
+    // Only rectangle and point draw function is needed
+        polyline: false, 
+        polygon: true,
+        circle: false,
+        circlemarker: false,
+        marker: true,
+        rectangle: false
+    }
+})) 
